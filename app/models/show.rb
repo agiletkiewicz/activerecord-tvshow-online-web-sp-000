ebdb7f434b2bb92a911ccead2473eca99ba4a670
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-24 20:32:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show 
-    rated = Show.find_by(rating: self.highest_rating)
-    rated.name
+    Show.find_by(rating: self.highest_rating)
   end
   
   
